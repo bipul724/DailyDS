1a2604a4e24ad43c675993cdd7c09fba348ded8e
bipul724 <bipulchamoli45@gmail.com>
2025-08-11 22:03:26 +0530

    Time: 2 ms (82.64%), Space: 45 MB (98.92%) - LeetHub

--- 0130-surrounded-regions/0130-surrounded-regions.java
+++ 0130-surrounded-regions/0130-surrounded-regions.java
@@ -34,11 +34,7 @@ class Solution {
             }
         }
 
-        for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++){
-                System.out.println(visited[i][j]+" ");
-            }
-        }
+        
         
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){

0e994887d2e6dd36b91ccef7ef8f9d3245ec5c81
bipul724 <bipulchamoli45@gmail.com>
2025-09-09 04:15:10 +0530

    Time: 23 ms (6.72%), Space: 41.6 MB (16.12%) - LeetHub

--- 1440-convert-integer-to-the-sum-of-two-no-zero-integers/1440-convert-integer-to-the-sum-of-two-no-zero-integers.java
+++ 1440-convert-integer-to-the-sum-of-two-no-zero-integers/1440-convert-integer-to-the-sum-of-two-no-zero-integers.java
@@ -1,10 +1,19 @@
 class Solution {
+    public boolean valid(int n){
+        while(n>0){
+            if(n%10==0){
+                return false;
+            }
+            n = n/10;
+        }
+        return true;
+    }
     public int[] getNoZeroIntegers(int n) {
         int ans[] = new int[2];
-        for(int i=1;i<=n;i++){
+        for(int i=1;i<n;i++){
             int a = n - i;
             int b = i;
-            if(!String.valueOf(a).contains("0") && !String.valueOf(b).contains("0")){
+            if(valid(a) && valid(b)){
                 ans[0]=a;
                 ans[1]=b;
             }

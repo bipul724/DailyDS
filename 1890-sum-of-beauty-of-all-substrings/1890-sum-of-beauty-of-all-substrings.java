f23af13b7ec31e43559a8a9dd773006539ecdd9b
bipul724 <bipulchamoli45@gmail.com>
2025-09-09 06:23:26 +0530

    Time: 58 ms (51.95%), Space: 43 MB (46.96%) - LeetHub

--- 1890-sum-of-beauty-of-all-substrings/1890-sum-of-beauty-of-all-substrings.java
+++ 1890-sum-of-beauty-of-all-substrings/1890-sum-of-beauty-of-all-substrings.java
@@ -1,29 +1,22 @@
 class Solution {
-    public int maxCount(int []freq){
-        int max = 0;
-        for(int i=0;i<freq.length;i++){
-            max = Math.max(max,freq[i]);
-        }
-        return max;
-    }
-    public int minCount(int []freq){
-        int min = Integer.MAX_VALUE;
-        for(int i=0;i<freq.length;i++){
-            if(freq[i]>0){
-                min = Math.min(min,freq[i]);
-            }
-            
-        }
-        return min;
-    }
     public int beautySum(String s) {
         int sum = 0;
         for(int i=0;i<s.length();i++){
             int []freq = new int[26];
             for(int j=i;j<s.length();j++){
                 freq[s.charAt(j)-'a']++;
-                int beauty = maxCount(freq)-minCount(freq);
-                sum += beauty;
+                int maxFreq = 0;
+                int minFreq = Integer.MAX_VALUE;
+
+                
+                for (int f : freq) {
+                    if (f > 0) {
+                        maxFreq = Math.max(maxFreq, f);
+                        minFreq = Math.min(minFreq, f);
+                    }
+                }
+
+                sum += (maxFreq - minFreq);
             }
         }
         return sum;

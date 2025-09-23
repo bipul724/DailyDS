3d36c26a0431dfb5171cef0f0f83b0f6f383a87a
bipul724 <bipulchamoli45@gmail.com>
2025-09-23 13:00:30 +0530

    Time: 1 ms (76.03%), Space: 41.3 MB (97.76%) - LeetHub

---
+++ 0165-compare-version-numbers/0165-compare-version-numbers.java
@@ -0,0 +1,133 @@
+class Solution {
+    public int compareVersion(String version1, String version2) {
+        int n1 = version1.length();
+        int n2 = version2.length();
+        int i = 0;
+        int j = 0;
+        char v1, v2;
+
+        while (i < n1 && j < n2) {
+            StringBuilder sb1 = new StringBuilder();
+            StringBuilder sb2 = new StringBuilder();
+            while (i<n1 && version1.charAt(i) != '.') {
+                v1 = version1.charAt(i);
+
+                sb1.append(v1);
+
+                i++;
+            }
+            i++;
+            while (j<n2 && version2.charAt(j) != '.') {
+                v2 = version2.charAt(j);
+
+                sb2.append(v2);
+
+                j++;
+            }
+            j++;
+
+            String st1 = sb1.toString();
+            String st2 = sb2.toString();
+            if ((Integer.valueOf(st1)) > (Integer.valueOf(st2))) {
+
+                return 1;
+
+            } else if (Integer.valueOf(st1) < Integer.valueOf(st2)) {
+
+                return -1;
+
+            }
+
+        }
+        // StringBuilder sb1 = new StringBuilder();
+        // StringBuilder sb2 = new StringBuilder();
+        // while (i<n1) {
+        //     v1 = version1.charAt(i);
+        //     if(v1=='.'){
+        //         String st1 = sb1.toString();
+        //         System.out.println(st1);
+
+        //         if(Integer.valueOf(st1)==0){
+        //             i++;
+        //         }
+        //         else{
+        //             return 1;
+        //         }
+        //         sb1 = new StringBuilder();
+        //     }
+        //     else{
+        //         sb1.append(v1);
+
+        //         i++;
+        //     }
+        //     // System.out.println(i);
+        // }
+
+        // while (j<n2) {
+        //     v2 = version2.charAt(j);
+        //     if(v2=='.'){
+        //         String st2 = sb2.toString();
+        //         if(Integer.valueOf(st2)==0){
+        //             j++;
+        //         }
+        //         else{
+        //             return -1;
+        //         }
+        //         sb2 = new StringBuilder();
+        //     }
+        //     else{
+        //         sb2.append(v2);
+
+        //         j++;
+        //     }
+        // }
+
+        while (i < n1 ) {
+            StringBuilder sb1 = new StringBuilder();
+            
+            while (i<n1 && version1.charAt(i) != '.') {
+                v1 = version1.charAt(i);
+
+                sb1.append(v1);
+
+                i++;
+            }
+            i++;
+            String st1 = sb1.toString();
+            
+            if ((Integer.valueOf(st1)) > 0) {
+
+                return 1;
+
+            } 
+
+    }
+
+    while (j < n2 ) {
+            StringBuilder sb2 = new StringBuilder();
+            
+            while (j<n2 && version2.charAt(j) != '.') {
+                v2 = version2.charAt(j);
+
+                sb2.append(v2);
+
+                j++;
+            }
+            j++;
+            String st2 = sb2.toString();
+            
+            if ((Integer.valueOf(st2)) > 0) {
+
+                return -1;
+
+            } 
+
+    }
+        
+
+
+
+        return 0;
+    }
+
+}

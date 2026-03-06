8df04389607551ef19b9fdef206705419cc6110f
bipul724 <bipulchamoli45@gmail.com>
2026-03-06 22:27:31 +0530

    Time: 6 ms (84.4%), Space: 47.2 MB (22.5%) - LeetHub

--- 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.java
+++ 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.java
@@ -1,15 +1,30 @@
 class Solution {
     public int firstUniqChar(String s) {
-        int dat[] = new int[26];
+        int arr[] = new int[26];
+        boolean flag[] = new boolean[26];
+        Arrays.fill(arr,-1);
+        
+
         for(int i=0;i<s.length();i++){
-            dat[s.charAt(i)-'a']++;
+            char ch = s.charAt(i);
+            if(arr[ch-'a']==-1){
+                arr[ch-'a'] = i;
+                flag[ch-'a']=true;
+            }
+            else{
+                flag[ch-'a']=false;
+            }
 
         }
-        for(int i=0;i<s.length();i++){
-            if(dat[s.charAt(i)-'a']==1){
-                return i;
+
+        int idx = Integer.MAX_VALUE;
+
+        for(int i=0;i<26;i++){
+            if(flag[i]==true){
+                idx = Math.min(idx,arr[i]);
             }
         }
-        return -1;
+
+        return idx == Integer.MAX_VALUE ? -1 : idx;
     }
 }

e189efcc5f420f739a9da8dd744124f429b2c4d8
bipul724 <bipulchamoli45@gmail.com>
2026-03-17 17:21:48 +0530

    Time: 4 ms (96.94%), Space: 43.8 MB (60.81%) - LeetHub

--- 1408-string-matching-in-an-array/1408-string-matching-in-an-array.java
+++ 1408-string-matching-in-an-array/1408-string-matching-in-an-array.java
@@ -1,6 +1,6 @@
 class Solution {
     public List<String> stringMatching(String[] words) {
-        //HashSet<String> set = new HashSet<>();
+        
         List<String> ans = new ArrayList<>();
         
         for(int i=0;i<words.length;i++){
@@ -11,9 +11,7 @@ class Solution {
                 }
             }
         }
-        // for(String str : set){
-        //     ans.add(str);
-        // }
+        
         return ans;
     }
 }

3f4949e4c1da33d97b02a2a909db7ffe15d584e8
bipul724 <bipulchamoli45@gmail.com>
2026-03-21 08:09:11 +0530

    Time: 33 ms (33.26%), Space: 84.3 MB (76.21%) - LeetHub

---
+++ 2762-continuous-subarrays/2762-continuous-subarrays.java
@@ -0,0 +1,22 @@
+class Solution {
+    public long continuousSubarrays(int[] nums) {
+        TreeMap<Integer, Integer> map = new TreeMap<>();
+        int i = 0;
+        int j = 0;
+        int n = nums.length;
+        long ans = 0;
+        while (j < n) {
+            map.put(nums[j], map.getOrDefault(nums[j], 0) + 1);
+            while (Math.abs(map.lastKey() - map.firstKey()) > 2) {
+                map.put(nums[i], map.get(nums[i]) - 1);
+                if (map.get(nums[i]) == 0) {
+                    map.remove(nums[i]);
+                }
+                i++;
+            }
+            ans += j - i + 1;
+            j++;
+        }
+        return ans;
+    }
+}

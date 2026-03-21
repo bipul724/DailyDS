0c6b62054e38f414acc31ca9d013a97d2f82d1e6
bipul724 <bipulchamoli45@gmail.com>
2026-03-21 12:53:06 +0530

    Time: 7 ms (8%), Space: 101 MB (21.41%) - LeetHub

---
+++ 3208-alternating-groups-ii/3208-alternating-groups-ii.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int numberOfAlternatingGroups(int[] colors, int k) {
+        int i = 0;
+        int j = 0;
+        int n = colors.length;
+        int ans = 0;
+        while(j<2*n){
+            if(j>0 && (Math.abs(colors[j%n]-colors[(j-1)%n])!=1)){
+                i=j;
+            }
+            if((j-i+1)>k){
+                i++;
+            }
+            if(((j-i+1)==k) && i<n){
+                ans++;
+            }
+            j++;
+        }
+        return ans;
+    }
+}

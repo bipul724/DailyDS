af6f0d5cb61822e061d525c77faff2f49ee71e17
bipul724 <bipulchamoli45@gmail.com>
2026-03-21 12:18:45 +0530

    Time: 18 ms (87.99%), Space: 88.3 MB (36.26%) - LeetHub

--- 2762-continuous-subarrays/2762-continuous-subarrays.java
+++ 2762-continuous-subarrays/2762-continuous-subarrays.java
@@ -1,20 +1,33 @@
 class Solution {
     public long continuousSubarrays(int[] nums) {
-        TreeMap<Integer, Integer> map = new TreeMap<>();
+        Deque<Integer> max = new ArrayDeque<>();
+        Deque<Integer> min = new ArrayDeque<>();
+        
         int i = 0;
         int j = 0;
         int n = nums.length;
         long ans = 0;
         while (j < n) {
-            map.put(nums[j], map.getOrDefault(nums[j], 0) + 1);
-            while (Math.abs(map.lastKey() - map.firstKey()) > 2) {
-                map.put(nums[i], map.get(nums[i]) - 1);
-                if (map.get(nums[i]) == 0) {
-                    map.remove(nums[i]);
+
+            while(!max.isEmpty() && nums[j]>nums[max.peekLast()]){
+                max.pollLast();
+            }
+            max.offerLast(j);
+            while(!min.isEmpty() && nums[j]<nums[min.peekLast()]){
+                min.pollLast();
+            }
+            min.offerLast(j);
+
+            while(Math.abs(nums[max.peekFirst()]-nums[min.peekFirst()])>2){
+                if(max.peekFirst() == i){
+                    max.pollFirst();
+                }
+                if(min.peekFirst() == i){
+                    min.pollFirst();
                 }
                 i++;
             }
-            ans += j - i + 1;
+            ans+=j-i+1;
             j++;
         }
         return ans;

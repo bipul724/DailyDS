c2bdfac949ad92a4e1722ed0c123c498035736cf
bipul724 <bipulchamoli45@gmail.com>
2026-03-23 14:31:08 +0530

    Time: 596 ms (2.63%), Space: 83.7 MB (99.58%) - LeetHub

--- 2302-count-subarrays-with-score-less-than-k/2302-count-subarrays-with-score-less-than-k.java
+++ 2302-count-subarrays-with-score-less-than-k/2302-count-subarrays-with-score-less-than-k.java
@@ -13,18 +13,17 @@ class Solution {
             sum+=nums[j];
             
             while((sum*(j-i+1))>=k){
-                //System.out.println("sum "+sum);
+               
                 System.out.println("i "+i+" j "+j);
 
                 sum-=nums[i];
                 i++;
             }
-            if((sum*(j-i+1))<k){
-                //System.out.println("sum "+sum);
+           
+                
                 ans+=j-i+1;
-                //System.out.println("i "+i+" j "+j);
-                //System.out.println("ans "+ans);
-            }
+                
+          
            
                 j++;
            

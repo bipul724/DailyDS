c6d07b40bf9329a28f05efc61fe53e26c14aba39
Bipul Chamoli <bipulchamoli45@gmail.com>
2026-07-16 13:35:13 +0530

    Time: 0 ms (100%), Space: 41.9 MB (72.76%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -1,15 +1,28 @@
 class Solution {
+    static int[] dp;
+
     public int climbStairs(int n) {
-        if(n==0 || n==1){
+        dp = new int[n + 1];
+        Arrays.fill(dp,-1);
+
+        return solve(n);
+    }
+
+    public int solve(int n) {
+        if (n == 0 || n == 1) {
             return 1;
         }
-        int dp[] = new int[n+1];
-        dp[0]=1;
-        dp[1]=1;
 
-        for(int i=2;i<=n;i++){
-            dp[i]=dp[i-1]+dp[i-2];
+        if (dp[n] != -1) {
+            return dp[n];
         }
+
+        
+        int left = solve(n - 1);
+        int right = solve(n - 2);
+
+        
+        dp[n] = left + right;
 
         return dp[n];
     }

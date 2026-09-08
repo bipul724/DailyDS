a1c34e85c42b3aff3d178173921008cb66fe5a09
Bipul Chamoli <bipulchamoli45@gmail.com>
2026-09-08 13:19:55 +0530

    Time: 36 ms (28.56%), Space: 59.1 MB (59.75%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -1,43 +1,37 @@
 class Solution {
-    public void twoSum(int nums[], List<List<Integer>> list, int target, int i, int j) {
-        while (i < j) {
-            int sum = nums[i] + nums[j] + target;
-            if (sum == 0) {
-                list.add(Arrays.asList(nums[i], nums[j], target));
-                i++;
-                j--;
-                while (i < j && nums[i] == nums[i - 1]) {
-                    i++;
-                }
-                while (i < j && nums[j] == nums[j + 1]) {
-                    j--;
-                }
-            }
-
-            if (sum > 0) {
-                j--;
-            } else if (sum < 0) {
-                i++;
-            }
-        }
-
-    }
-
     public List<List<Integer>> threeSum(int[] nums) {
-        List<List<Integer>> list = new ArrayList<>();
-        Arrays.sort(nums);
+        ArrayList<List<Integer>> ans = new ArrayList<>();
         int n = nums.length;
-        if (n < 3) {
-            return list;
-        }
+        Arrays.sort(nums);
+        for(int i=0;i<n-2;i++){
+            if(i>0 && nums[i]==nums[i-1]){
+                    continue;
+                }
+            int left = i+1;
+            int right = n-1;
+            while(left<right){
+                
+                int sum = nums[i] + nums[left] + nums[right];
+                if(sum==0){
+                    ans.add(new ArrayList<>(Arrays.asList(nums[i],nums[left],nums[right])));
+                    left++;
+                    right--;
 
-        for (int i = 0; i < n - 2; i++) {
-            if (i > 0 && nums[i] == nums[i - 1]) {
-                continue;
+                    while(left<right && nums[left]==nums[left-1]){
+                        left++;
+                    }
+                    while(left<right && nums[right]==nums[right+1]){
+                        right--;
+                    }
+                }
+                else if(sum>0){
+                    right--;
+                }
+                else{
+                    left++;
+                }
             }
-            twoSum(nums, list, nums[i], i + 1, n - 1);
         }
-
-        return list;
+        return ans;
     }
 }
